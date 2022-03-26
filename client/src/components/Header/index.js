e8f864deed01d64b50065a2be3ebcb6ed46f6afc
sboucher <sboucher2011@gmail.com>
2022-03-26 15:22:21 -0400

    updated header and footer

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,39 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import 'bulma/css/bulma.min.css';
 import Auth from '../../utils/auth';
-
+//import images
+import Logo from "../../assets/Header/logo.png"
 const Header = () => {
   const logout = event => {
     event.preventDefault();
     Auth.logout();
   };
-
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/">
-          <h1>Deep Thoughts</h1>
-        </Link>
-
-        <nav className="text-center">
-          {Auth.loggedIn() ? (
-            <>
-              <Link to="/profile">Me</Link>
-              <a href="/" onClick={logout}>
-                Logout
+    <header>
+      <nav className="navbar">
+        {Auth.loggedIn() ? (
+          <>
+            <div className='navbar-brand'>
+              <a className='navbar-item' href="/">
+                <img src={Logo} alt="logo" />Sparkling Spaces
               </a>
-            </>
-          ) : (
-            <>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-            </>
-          )}
-        </nav>
-      </div>
+            </div>
+            <div className='navbar-menu is-active'>
+              <div className='navbar-end'>
+                <Link to="/" onClick={logout} className='navbar-item'>
+                  Logout
+                </Link>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className='navbar-brand'>
+              <a className='navbar-item' href="/">
+                <img src={Logo} alt="logo" />Sparkling Spaces
+              </a>
+            </div>
+            <div className='navbar-menu'>
+              <div className='navbar-end'>
+                <Link to="/login" className='navbar-item'>
+                  Login
+                </Link>
+                <Link to="/signup" className='navbar-item'>
+                  Sign Up
+                </Link>
+              </div>
+            </div>
+          </>
+        )}
+      </nav>
     </header>
   );
 };
-
 export default Header;
